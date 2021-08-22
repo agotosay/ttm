2e72e10d1e8f81634131548d411324b35cd97366
Agus Kristianto <agus.kristianto88@gmail.com>
2021-08-22 10:32:07 +0700

    move review, leave review, related product in detail product page

--- pages/detail-product.vue
+++ pages/detail-product.vue
@@ -6,7 +6,8 @@
     <!-- navbar -->
     <Navbar />
 
-    <section>
+    <!-- desc product -->
+    <section class="position-relative">
       <b-container>
         <b-row>
           <b-col cols="5" class="detail-product">
@@ -108,7 +109,6 @@
                 />
               </div>
               <!-- toggle color -->
-              <!-- toggle color -->
               <div class="main-color" v-if="secondColor">
                 <div class="color dark-brown"></div>
                 <div class="color jade"></div>
@@ -145,7 +145,6 @@
                 alt="image"
                 />
               </div>
-              <!-- toggle color -->
               <!-- toggle color -->
               <div class="main-color" v-if="sleeveColor">
                 <div class="color dark-brown"></div>
@@ -330,24 +329,22 @@
                 </b-collapse>
               </div>
             </div>
-            <!-- review -->
-            <div class="title mt-2 text-center mb-1">REVIEW</div>
-            <div class="user-review">
-            <p>I was blown away by the tiny packing size and the very thin and light material. It certainly is the lightest hammock I have ever come across. The material feels like very fine silk, thin as glad wrap. However, it does not feel sweaty and does not seem to be noisy. For its minimalistic design it is very spacious and comfortable, for daily use where weight is no issue, my original TTTM hammock will still be my first choice though. I am curious to test the durability over time. At this point I would be super cautious around prickly plants in order not to have it ripped. Certainly a good choice when every gram counts.</p>
-            <div class="d-flex">
-              <div>
-                <b-avatar></b-avatar>
-              </div>
-              <div class="ml-2">
-                <div class="review-name mb-1">Martin Lawrence</div>
-                <div class="product-rating">
-                  <i class="fas fa-star" v-for="indexRating in 5" :key="indexRating" :class="{'text-warning': indexRating <= 5}"></i>
-                </div>
-                
-                <div class="review-role">Moonquilt</div>
-              </div>
-            </div>
-            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+      <div class="badge-fast for-product">
+        <b-link href="">10-year <br>warranty</b-link>  
+      </div>
+    </section>
+
+    <!-- review -->
+    <section class="reviews">
+      <b-container>
+        <div class="title text-center mb-2">
+          REVIEWS
+        </div>
+        <div>
+          <VueSlickCarousel v-bind="reviews" :arrows="true" :dots="true">
             <div class="user-review">
               <p>I was blown away by the tiny packing size and the very thin and light material. It certainly is the lightest hammock I have ever come across. The material feels like very fine silk, thin as glad wrap. However, it does not feel sweaty and does not seem to be noisy. For its minimalistic design it is very spacious and comfortable, for daily use where weight is no issue, my original TTTM hammock will still be my first choice though. I am curious to test the durability over time. At this point I would be super cautious around prickly plants in order not to have it ripped. Certainly a good choice when every gram counts.</p>
               <div class="d-flex">
@@ -364,13 +361,71 @@
                 </div>
               </div>
             </div>
-            <!-- leave review -->
-            <b-card class="text-center mt-2">
-              <div class="leave-review">
-                <div class="title">tell everyone about you experience</div>
+            <div class="user-review">
+              <p>I was blown away by the tiny packing size and the very thin and light material. It certainly is the lightest hammock I have ever come across. The material feels like very fine silk, thin as glad wrap. However, it does not feel sweaty and does not seem to be noisy. For its minimalistic design it is very spacious.</p>
+              <div class="d-flex">
+                <div>
+                  <b-avatar></b-avatar>
+                </div>
+                <div class="ml-1">
+                  <div class="review-name mb-1">Martin Lawrence</div>
+                  <div class="product-rating">
+                    <i class="fas fa-star" v-for="indexRating in 5" :key="indexRating" :class="{'text-warning': indexRating <= 5}"></i>
+                  </div>
+                  
+                  <div class="review-role">Moonquilt</div>
+                </div>
+              </div>
+            </div>
+            <div class="user-review">
+              <p>I was blown away by the tiny packing size and the very thin and light material. It certainly is the lightest hammock I have ever come across. The material feels like very fine silk, thin as glad wrap. However, it does not feel sweaty and does not seem to be noisy. For its minimalistic design it is very spacious. I was blown away by the tiny packing size and the very thin and light material. It certainly is the lightest hammock I have ever come across. The material feels like very fine silk, thin as glad wrap.</p>
+              <div class="d-flex">
+                <div>
+                  <b-avatar></b-avatar>
+                </div>
+                <div class="ml-1">
+                  <div class="review-name mb-1">Martin Lawrence</div>
+                  <div class="product-rating">
+                    <i class="fas fa-star" v-for="indexRating in 5" :key="indexRating" :class="{'text-warning': indexRating <= 5}"></i>
+                  </div>
+                  
+                  <div class="review-role">Moonquilt</div>
+                </div>
+              </div>
+            </div>
+            <div class="user-review">
+              <p>I was blown away by the tiny packing size and the very thin and light material. It certainly is the lightest hammock I have ever come across. The material feels like very fine silk, thin as glad wrap. However, it does not feel sweaty and does not seem to be noisy. For its minimalistic design it is very spacious and comfortable, for daily use where weight is no issue, my original TTTM hammock will still be my first choice though. I am curious to test the durability over time. At this point I would be super cautious around prickly plants in order not to have it ripped. Certainly a good choice when every gram counts. I was blown away by the tiny packing size and the very thin and light material. It certainly is the lightest hammock I have ever come across. The material feels like very fine silk, thin as glad wrap. However, it does not feel sweaty and does not seem to be noisy. For its minimalistic design it is very spacious and comfortable.</p>
+              <div class="d-flex">
+                <div>
+                  <b-avatar></b-avatar>
+                </div>
+                <div class="ml-1">
+                  <div class="review-name mb-1">Martin Lawrence</div>
+                  <div class="product-rating">
+                    <i class="fas fa-star" v-for="indexRating in 5" :key="indexRating" :class="{'text-warning': indexRating <= 5}"></i>
+                  </div>
+                  <div class="review-role">Moonquilt</div>
+                </div>
+              </div>
+            </div>
+          </VueSlickCarousel>
+        </div>
+      </b-container>
+    </section>
+
+    <!-- leave review -->
+    <section>
+      <b-container>
+        <b-card class="card-pad">
+          <div class="leave-review">
+            <b-row>
+              <b-col cols="6">
+                <div class="title mb-1">tell everyone about you experience</div>
                 <p>We’ll Be Grateful If You Can Let Us Know About Your Experience In Using Our Products. Please Leave Us Your Thoughts In The Form Of A Review. As Per Our System Requirement, The Review Section Is Moderated, So If You Haven’t Purchased Our Products, Your Identification Info Will Not Match Our Customer Records, And Your Review Couldn’t Be Processed.</p>
+              </b-col>
+              <b-col cols="6">
                 <div class="mb-1">
-                  <small class="mr-1">Your Rating</small><i class="fas fa-star" v-for="indexRating in 5" :key="indexRating" :class="{'text-warning': indexRating <= 5}"></i>
+                  <small class="mr-1">Your Rate</small><i class="fas fa-star" v-for="indexRating in 5" :key="indexRating" :class="{'text-warning': indexRating <= 5}"></i>
                 </div>
                 <b-form-group>
                   <b-form-input
@@ -395,82 +450,145 @@
                     SUBMIT
                   </b-button>
                 </b-form-group>
-              </div>
-            </b-card>
-            <!-- other product -->
-            <b-row class="mt-2">
-              <b-col cols="6">
-                <div class="our-products">
-                  <img
-                  class="d-block img-fluid w-100"
-                  src="/product-1.jpg"
-                  alt="image"
-                  />
-                  <div class="product-title mb-1">
-                    TRAVEL HAMMOCK
-                  </div>
-                  <div class="product-rating">
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                  </div>
-                  <div class="product-price">40.99 &euro;</div>
-                  <b-button variant="thirdy" class="w-100 d-flex align-items-center justify-content-center mt-2 radius-corner" @click="$router.push('/detail-product')">
-                    VIEW DETAIL
-                    <img 
-                    src="/arrow-right.svg" 
-                    width="6"
-                    alt="image" 
-                    class="ml-1" />
-                  </b-button>
-                </div>
-              </b-col>
-              <b-col cols="6">
-                <div class="our-products">
-                  <img
-                  class="d-block img-fluid w-100"
-                  src="/product-1.jpg"
-                  alt="image"
-                  />
-                  <div class="product-title mb-1">
-                    TRAVEL HAMMOCK
-                  </div>
-                  <div class="product-rating">
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                    <i class="fas fa-star text-warning"></i>
-                  </div>
-                  <div class="product-price">40.99 &euro;</div>
-                  <b-button variant="thirdy" class="w-100 d-flex align-items-center justify-content-center mt-2 radius-corner" @click="$router.push('/detail-product')">
-                    VIEW DETAIL
-                    <img 
-                    src="/arrow-right.svg" 
-                    width="6"
-                    alt="image" 
-                    class="ml-1" />
-                  </b-button>
-                </div>
               </b-col>
             </b-row>
-            <div>
-              <b-button variant="primary" class="w-100 d-flex align-items-center justify-content-center mt-2" @click="$router.push('/detail-product')">
-                VIEW ALL PRODUCTS
-                <img 
-                src="/arrow-right.svg" 
-                width="6"
-                alt="image" 
-                class="ml-1" />
-              </b-button>
-            </div>
-          </b-col>
-        </b-row>
+          </div>
+        </b-card>
       </b-container>
     </section>
 
+    <!-- other product -->
+    <b-container>
+      <div class="title text-center mb-2">
+        Related Products
+      </div>
+      <b-row class="mt-2">
+        <b-col cols="3">
+          <div class="our-products">
+            <img
+            class="d-block img-fluid w-100"
+            src="/product-1.jpg"
+            alt="image"
+            />
+            <div class="product-title mb-1">
+              TRAVEL HAMMOCK
+            </div>
+            <div class="product-rating">
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+            </div>
+            <div class="product-price">40.99 &euro;</div>
+            <b-button variant="thirdy" class="w-100 d-flex align-items-center justify-content-center mt-2 radius-corner" @click="$router.push('/detail-product')">
+              VIEW DETAIL
+              <img 
+              src="/arrow-right.svg" 
+              width="6"
+              alt="image" 
+              class="ml-1" />
+            </b-button>
+          </div>
+        </b-col>
+        <b-col cols="3">
+          <div class="our-products">
+            <img
+            class="d-block img-fluid w-100"
+            src="/product-1.jpg"
+            alt="image"
+            />
+            <div class="product-title mb-1">
+              TRAVEL HAMMOCK
+            </div>
+            <div class="product-rating">
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+            </div>
+            <div class="product-price">40.99 &euro;</div>
+            <b-button variant="thirdy" class="w-100 d-flex align-items-center justify-content-center mt-2 radius-corner" @click="$router.push('/detail-product')">
+              VIEW DETAIL
+              <img 
+              src="/arrow-right.svg" 
+              width="6"
+              alt="image" 
+              class="ml-1" />
+            </b-button>
+          </div>
+        </b-col>
+        <b-col cols="3">
+          <div class="our-products">
+            <img
+            class="d-block img-fluid w-100"
+            src="/product-1.jpg"
+            alt="image"
+            />
+            <div class="product-title mb-1">
+              TRAVEL HAMMOCK
+            </div>
+            <div class="product-rating">
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+            </div>
+            <div class="product-price">40.99 &euro;</div>
+            <b-button variant="thirdy" class="w-100 d-flex align-items-center justify-content-center mt-2 radius-corner" @click="$router.push('/detail-product')">
+              VIEW DETAIL
+              <img 
+              src="/arrow-right.svg" 
+              width="6"
+              alt="image" 
+              class="ml-1" />
+            </b-button>
+          </div>
+        </b-col>
+        <b-col cols="3">
+          <div class="our-products">
+            <img
+            class="d-block img-fluid w-100"
+            src="/product-1.jpg"
+            alt="image"
+            />
+            <div class="product-title mb-1">
+              TRAVEL HAMMOCK
+            </div>
+            <div class="product-rating">
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+              <i class="fas fa-star text-warning"></i>
+            </div>
+            <div class="product-price">40.99 &euro;</div>
+            <b-button variant="thirdy" class="w-100 d-flex align-items-center justify-content-center mt-2 radius-corner" @click="$router.push('/detail-product')">
+              VIEW DETAIL
+              <img 
+              src="/arrow-right.svg" 
+              width="6"
+              alt="image" 
+              class="ml-1" />
+            </b-button>
+          </div>
+        </b-col>
+      </b-row>
+      <div class="text-center">
+        <b-button variant="primary" class="mt-2 mb-3" @click="$router.push('/detail-product')">
+          VIEW ALL PRODUCTS
+          <img 
+          src="/arrow-right.svg" 
+          width="6"
+          alt="image" 
+          class="ml-1" />
+        </b-button>
+      </div>
+    </b-container>
+
+    <!-- price total -->
     <div class="price-total">
       <b-container>
         <b-row>
@@ -709,7 +827,13 @@ export default {
       sleeveColor: false,
       onSleeveSelect: '',
       selectedAcc1: '',
-      selectedAcc2: ''
+      selectedAcc2: '',
+      reviews: {
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 2,
+        arrows: true,
+      }
     }
   },
   methods: {
